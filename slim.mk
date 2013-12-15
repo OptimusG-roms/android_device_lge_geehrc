cbcf7200eb2243e9e078db59253caf2ce311d2a9
Kevindeving <98kevchen@gmail.com>
2013-12-15 19:12:08 +0100

    Initial Slimkat bringup

--- slim.mk
+++ slim.mk
@@ -1,30 +1,17 @@
-#
-# Copyright 2012 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the long list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/slim/config/common.mk)
+
+# Inherit led flash settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/geehrc/device.mk)
@@ -32,7 +19,10 @@ $(call inherit-product, device/lge/geehrc/device.mk)
 # Inherit from common hardware-specific part of the product configuration
 $(call inherit-product, device/lge/gee-common/gee-common.mk)
 
-PRODUCT_NAME := omni_geehrc
+# Release name
+PRODUCT_RELEASE_NAME := geehrc
+
+PRODUCT_NAME := slim_geehrc
 PRODUCT_DEVICE := geehrc
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := Optimus G
